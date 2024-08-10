b2c5e0429d24545e525a556189ee5dab4fec4d37
felicity0000 <anzoejohn@gmail.com>
2024-08-10 10:20:57 +0300

    scraping games

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,11 +4,11 @@ import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
-    <div className="border-b-2 border-b-red-500 py-6">
+    <div className="relative-container border-b-2 border-b-red-500 py-6 left-0 top-0 right-0 sticky z-10 bg-transparent bg-opacity-60 backdrop-blur-lg ">
       <div className="container mx-auto flex justify-between items-center">
         <Link
           to="/"
-          className="text-3xl font-bold tracking-tight text-red-500"
+          className="text-3xl font-bold tracking-tight text-red-500 border rounded-lg p-2 bg-white"
         >
           Games-To-Scrape
         </Link>
